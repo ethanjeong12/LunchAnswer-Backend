6342216ff576cb61763be96101959620bf44b745
shrhkddh <shrhkddh0244@gmail.com>
2020-08-21 16:56:20 +0900

    [Add]menu app models.py 작성

---
+++ menu/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Menu(models.Model):
+    category     = models.CharField(max_length = 50)
+    name         = models.CharField(max_length = 100)
+    contain_meet = models.BooleanField()
+    seafood      = models.BooleanField()
+    hot          = models.BooleanField()
+    noodle       = models.BooleanField()
+    soup         = models.BooleanField()
+    greasy       = models.BooleanField()
+    cool         = models.BooleanField()
+
+    class Meta:
+        db_table = 'menus'
