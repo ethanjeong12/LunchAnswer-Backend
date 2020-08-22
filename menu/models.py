e3acf3deeed198aeb89a80da00ee1299114b1fce
shrhkddh <shrhkddh0244@gmail.com>
2020-08-22 18:22:38 +0900

    [refactor] models.py, settings.py 수정

--- menu/models.py
+++ menu/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Menu(models.Model):
     category     = models.CharField(max_length = 50)
     name         = models.CharField(max_length = 100)
-    contain_meet = models.BooleanField()
+    contain_meat = models.BooleanField()
     seafood      = models.BooleanField()
     hot          = models.BooleanField()
     noodle       = models.BooleanField()
